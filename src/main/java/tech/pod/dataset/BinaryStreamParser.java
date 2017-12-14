ab8556cff688db809bcfaf93f6af8f247b8b63ca
Cole Christini <cole.christini@gmail.com>
2017-12-14 09:56:42 -0500

    UPDOOT

--- src/main/java/tech/pod/dataset/BinaryStreamParser.java
+++ src/main/java/tech/pod/dataset/BinaryStreamParser.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.*;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.*;
 import java.util.logging.Level;
