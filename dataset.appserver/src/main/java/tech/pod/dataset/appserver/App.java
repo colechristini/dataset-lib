1d6c8038e57378a2c8c884e712f7ecba5d3df4dd
Cole Christini <cole.christini@gmail.com>
2018-01-03 01:35:54 -0500

    AppServer

---
+++ dataset.appserver/src/main/java/tech/pod/dataset/appserver/App.java
@@ -0,0 +1,13 @@
+package tech.pod.dataset.appserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
