98b942460100a3193429d20de7e4b0f0ad7efa61
Cole Christini <cole.christini@gmail.com>
2018-02-01 12:34:19 -0500

    Files

--- dataset.core/src/main/java/tech/pod/dataset/RunAlg.java
+++ dataset.core/src/main/java/tech/pod/dataset/RunAlg.java
@@ -1,13 +1,13 @@
 package tech.pod.dataset;
 
 public class RunAlg {
-    DoAlg Do = new DoAlg();
+    DoAlg doAlg = new DoAlg();
     String[] args;
     RunAlg(String[] args) {
         this.args = args;
     }
     
     void run() {
-        Do.doAlg(args);
+    doAlg.doAlg(args);
     }
 }
---
+++ dataset.ims/src/tech/pod/dataset/ims/Property.java
@@ -0,0 +1,28 @@
+public class Property<T> {
+    String name,type;
+    T value;
+    boolean setImmutable;
+    Propery(String name, String type, T value,boolean setImmutable){
+        this.name=name;
+        this.type=type;
+        this.value=value;
+        this.setImmutable=setImmutable;
+    }
+    public T getValue(){
+        return value;
+    }
+    public void setValue(T value) throws UnsupportedOperationException{
+        if(!setImmutable){
+            this.value=value;
+        }
+        else{
+            throw new UnsupportedOperationException;
+        }
+    }
+    public String getName(){
+        return name
+    }
+    public String getType(){
+        return type;
+    }
+}
