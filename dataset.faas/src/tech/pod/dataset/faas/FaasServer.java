33b95eedec2a14d3f6591b4facf11f2f22c5e2b3
Cole Christini <cole.christini@gmail.com>
2018-02-22 10:17:51 -0500

    FaaSServer done

--- dataset.faas/src/tech/pod/dataset/faas/FaasServer.java
+++ dataset.faas/src/tech/pod/dataset/faas/FaasServer.java
@@ -1,12 +1,15 @@
 package tech.pod.dataset.faas;
 
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class FaasServer {
@@ -19,23 +22,79 @@ public class FaasServer {
         this.port = port;
         socket = new ServerSocket(port);
     }
-    public void accept(){
-        Callable functionCall=()->{
-            int location=sockets.size()-1;
-            ObjectInputStream stream= sockets.get(location).getInputStream();
-            Object[] objects=stream.readObject();
-            try{
-                Object[] extraparams=Arrays.copyOfRange(objects, 3, objects.length-1);
-               List<? extends Object> returns=functions.get(objects[0].toString()).invoke(objects[1].toString, objects[2], extraparams) ;
+    public void accept() {
+        Runnable addFunction = () -> {
+           
+            int location = sockets.size() - 1;
+            Socket s=sockets.get(location);
+            ObjectInputStream stream = s.getInputStream();
+            Object[] objects = stream.readObject();
+            sockets.remove(location);
+            Thread thread = Thread.currentThread();
+            Class < InvokeableFunction > caster = new Class < InvokeableFunction > ();
+            InvokeableWrapper wrapper = new InvokeableWrapper(caster.cast(objects[1]), objects[2].toString());
+            sockets.remove(s);
+        };
+        Runnable removeFunction = () -> {
+            int location = sockets.size() - 1;
+            Socket s=sockets.get(location);
+            ObjectInputStream stream =  s.getInputStream();
+            Object[] objects = stream.readObject();
+            Thread thread = Thread.currentThread();
+            if (functions.get(objects[1].toString()).getKeyLocation(objects[2].toString()) != 0) {
+                functions.remove(objects[1].toString());
+                sockets.remove(s);
             }
-            catch(KeyNotAuthorizedException kne){
+            else{
+                sockets.remove(s);
+                throw new KeyNotAuthorizedException(objects[2].toString());
+            }
+            
+        };
+        Runnable functionCall = () -> {
+            int location = sockets.size() - 1;
+            Socket s=sockets.get(location);
+            ObjectInputStream stream = s.getInputStream();
+            Object[] objects = stream.readObject();
+            Thread thread = Thread.currentThread();
+            try {
+                List < ? extends Object > returns;
+                if (objects.length >= 5) {
+                    Object[] extraparams = Arrays.copyOfRange(objects, 4, objects.length - 1);
+                    returns = functions.get(objects[1].toString()).invoke(objects[2].toString, objects[3], extraparams);
+                } else {
+                    returns = functions.get(objects[1].toString()).invoke(objects[2].toString, objects[3]);
+                }
+
+                ObjectOutputStream output = s.getOutputStream();
+                output.writeObject(returns);
+                sockets.remove(s);
+            } catch (KeyNotAuthorizedException kna) {
 
             }
         };
-        while(!stop){
+        while (!stop) {
+            Socket tempSocket = socket.accept();
+            if (tempSocket != null) {
+                sockets.add(tempSocket);
+                ExecutorService service = Executors.newSingleThreadExecutor();
+                ObjectInputStream stream = sockets.get(location).getInputStream();
+                Object[] objects = stream.readObject();
 
+                if (objects[0].toString() == "invoke") {
+                    service.execute(functionCall);
+                }
+            
+                else if (objects[0].toString() == "add") {
+                service.execute(addFunction);
+                }
+
+                else if (objects[0].toString() == "remove") {
+                    service.execute(removeFunction);
+            }
         }
-        stop=false;
     }
-    
+    stop = false;
+}
+
 }
--- dataset.faas/src/tech/pod/dataset/faas/InvokeableWrapper.java
+++ dataset.faas/src/tech/pod/dataset/faas/InvokeableWrapper.java
@@ -1,27 +1,40 @@
 package tech.pod.dataset.faas;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class InvokeableWrapper<R> {
+public class InvokeableWrapper {
     InvokeableFunction function;
     ConcurrentHashMap < String, Integer > keys = new ConcurrentHashMap < String, Integer > ();
-    String type;
-    InvokeableWrapper(InvokeableFunction function, String startingKey,String type) {
-        this.type=type;
+    List<String> keyList=new ArrayList<String>();
+    InvokeableWrapper(InvokeableFunction function, String startingKey) {
         this.function = function;
         keys.put(startingKey, 0);
+        keyList.add(startingKey);
     }
     public void putKey(String key) {
         keys.put(key, 0);
+        keyList.add(key);
     }
     public void removeKey(String key){
         keys.remove(key);
+        keyList.remove(key);
     }
     public Integer getKeyValue(String key){
         return keys.get(key);
     }
+    public Integer getKeyLocation(String key){
+        for(int i=0;i<keyList.size();i++){
+            if(key==keyList.get(i)){
+                Integer r=i;
+                return r;
+            }
+        }
+        return null;
+    }
     @SafeVarargs
-    public final <T> List<R> invoke(String key, T parameter, T... parameters) throws KeyNotAuthorizedException{
+    public final <T> List<? extends Object> invoke(String key, T parameter, T... parameters) throws KeyNotAuthorizedException{
         if(keys.containsKey(key)){
             keys.replace(key, keys.get(key)+1);
             return function.invoke(parameter, parameters);
