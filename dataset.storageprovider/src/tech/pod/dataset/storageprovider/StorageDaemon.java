2a6459ba7fd672228f545b5c25409a8f3d20a627
Cole Christini <cole.christini@gmail.com>
2018-06-19 11:23:07 -0400

    Finished StorageDaemon get

--- dataset.storageprovider/src/tech/pod/dataset/storageprovider/StorageDaemon.java
+++ dataset.storageprovider/src/tech/pod/dataset/storageprovider/StorageDaemon.java
@@ -1,7 +1,11 @@
 package tech.pod.dataset.storageprovider;
 
 import java.nio.*;
+import java.nio.channels.FileChannel;
 import java.nio.channels.SocketChannel;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
@@ -18,21 +22,41 @@ import java.util.concurrent.TimeUnit;
 //StorageDaemons run on individual servers, managing the files on the server and replicationg them to all servers within the stripe
 public class StorageDaemon{
     ArrayList<Integer,Integer> stripeIPs=new ArrayList<Integer,Integer>();
-    ConcurrentHashMap<String,String> fileNames=new ConcurrentHashMap<String>();
+    ConcurrentHashMap<String,String> fileLocs=new ConcurrentHashMap<String,String>();
+    ConcurrentHashMap<String,Integer> fileSizes=new ConcurrentHashMap<String,Integer>();
     boolean isActive;
-    SocketChannel socket=SocketChannel.open();;
-    SocketAddress daemonIP;
-    Socket
-    StorageDaemon(boolean isActive,SocketAddress daemonIP){
+    InetSocketAddress daemonIP;
+    Socket command=Socket.open();
+    InetSocketAddress commandIP;
+    StorageDaemon(boolean isActive,InetSocketAddress daemonIP,InetSocketAddress commandIP){
         this.isActive=isActive;
         this.daemonIP=daemonIP;
+        this.commandIP=commandIP;
     }
     public void start(){
-        socket.bind(daemonIP);
+        command.bind(commandIP);
     }
     public void recieve(){
         Runnable reciever=()->{
-            
+            PrintWriter out=new PrintWriter(command.getOutputStream(),true);
+            String commandString=out.toString();
+            if(command){
+                if(command==get){
+                    String[] components=commandString.split(":");
+                    RandomAccessFile file=new RandomAccessFile(fileNames.get(components[1],"r"));
+                    ByteBuffer bb=ByteBuffer.allocate(fileSizes.get(components[1]));
+                    FileChannel fileChannel=file.getChannel();
+                    int bytesRead=fileChannel.read(bb);
+                    bb.flip();
+                    InetSocketAddress remote=new InetSocketAddress(InetAddress.getByName(components[2]));
+                    SocketChannel socket=SocketChannel.open();
+                    socket.bind(daemonIP);
+                    socket.connect(remote);
+                    socket.finishConnect();
+                    socket.write(bb);
+                    bb.flip();
+                }
+            }
         };
     }
 }
