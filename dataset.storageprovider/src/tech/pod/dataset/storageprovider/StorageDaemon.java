d2b89755dd29abec463729ca5e53697e1d66a4a3
Cole Christini <cole.christini@gmail.com>
2018-06-19 12:04:21 -0400

    Finished StorageDaemon

--- dataset.storageprovider/src/tech/pod/dataset/storageprovider/StorageDaemon.java
+++ dataset.storageprovider/src/tech/pod/dataset/storageprovider/StorageDaemon.java
@@ -19,19 +19,23 @@ import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-//StorageDaemons run on individual servers, managing the files on the server and replicationg them to all servers within the stripe
+//StorageDaemons run on individual servers, managing the files on the server and replicationg them to all servers within the stripe.
 public class StorageDaemon{
-    ArrayList<Integer,Integer> stripeIPs=new ArrayList<Integer,Integer>();
+    ArrayList<Integer,InetAddress> stripeIPs=new ArrayList<Integer,Integer>();
     ConcurrentHashMap<String,String> fileLocs=new ConcurrentHashMap<String,String>();
     ConcurrentHashMap<String,Integer> fileSizes=new ConcurrentHashMap<String,Integer>();
     boolean isActive;
     InetSocketAddress daemonIP;
     Socket command=Socket.open();
     InetSocketAddress commandIP;
-    StorageDaemon(boolean isActive,InetSocketAddress daemonIP,InetSocketAddress commandIP){
+    int defaultBufferSize;
+    String saveLocation;
+    StorageDaemon(boolean isActive,InetSocketAddress daemonIP,InetSocketAddress commandIP,int defaultBufferSize,String saveLocation){
         this.isActive=isActive;
         this.daemonIP=daemonIP;
         this.commandIP=commandIP;
+        this.defaultBufferSize=defaultBufferSize;
+        this.saveLocation=saveLocation;
     }
     public void start(){
         command.bind(commandIP);
@@ -40,23 +44,49 @@ public class StorageDaemon{
         Runnable reciever=()->{
             PrintWriter out=new PrintWriter(command.getOutputStream(),true);
             String commandString=out.toString();
-            if(command){
-                if(command==get){
+            if(commandString){
+                if(commandString==get){
                     String[] components=commandString.split(":");
-                    RandomAccessFile file=new RandomAccessFile(fileNames.get(components[1],"r"));
-                    ByteBuffer bb=ByteBuffer.allocate(fileSizes.get(components[1]));
+                    RandomAccessFile file=new RandomAccessFile(fileNames.get(components[1]),"r");
+                    ByteBuffer buffer=ByteBuffer.allocate(fileSizes.get(components[1]));
                     FileChannel fileChannel=file.getChannel();
                     int bytesRead=fileChannel.read(bb);
-                    bb.flip();
+                    buffer.flip();
                     InetSocketAddress remote=new InetSocketAddress(InetAddress.getByName(components[2]));
                     SocketChannel socket=SocketChannel.open();
                     socket.bind(daemonIP);
                     socket.connect(remote);
                     socket.finishConnect();
-                    socket.write(bb);
-                    bb.flip();
+                    socket.write(buffer);
+                }
+                else if(commandString==write){
+                    String[] components=commandString.split(":");
+                    RandomAccessFile file=new RandomAccessFile(saveLocation="/"+components[2],"w");
+                    fileLocs.put(components[1], components[2]);
+                    ByteBuffer buffer=ByteBuffer.allocate(defaultBufferSize);
+                    InetSocketAddress remote=new InetSocketAddress(InetAddress.getByName(components[3]));
+                    SocketChannel socket=SocketChannel.open();
+                    socket.bind(daemonIP);
+                    socket.connect(remote);
+                    socket.read(buffer);
+                    buffer.flip(); 
+                    FileChannel channel=file.getChannel();
+                    channel.write(buffer);
+                    if(isActive){
+                        for(int i=1;i<stripeIps.size();i++){
+                            Socket commandSenderSocket=new Socket(stripeIPs.get(i),10000);
+                            OutputStream stream=commandSenderSocket.getOutputStream();
+                            PrintWriter writer=new PrintWriter(stream);
+                            writer.write(commandString);
+                            socket.connect(stripeIPs.get(i));
+                            socket.write(buffer);
+                        }
+                    }
                 }
             }
+        };
+        Runnable stripeReciever=()->{
+            
         };
     }
 }
