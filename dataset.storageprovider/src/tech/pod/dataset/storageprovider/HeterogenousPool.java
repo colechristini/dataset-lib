c7746fa49a84cd9989d27df785f5c00cb34ef70c
Cole Christini <cole.christini@gmail.com>
2018-09-01 18:35:54 +0200

    1

--- dataset.storageprovider/src/tech/pod/dataset/storageprovider/HeterogenousPool.java
+++ dataset.storageprovider/src/tech/pod/dataset/storageprovider/HeterogenousPool.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-//StoragePool with per-stripe tiering instead of a homogeneous pool where every server is the same, containing all tiers. Individual servers run StorageDaemons to manage and send data.
+//StoragePool with per-stripe tiering instead of a homogeneous pool, wherein every server is the same, containing all tiers. Individual servers run StorageDaemons to manage and send data.
 public class HeterogenousPool implements StoragePool {
     List < List < SocketAddress > > storageDaemons = new ArrayList < ArrayList < SocketAddress > > ();
     List < List < SocketAddress > > storageDaemonCommandAddresses = new ArrayList < ArrayList < SocketAddress > > ();
@@ -20,13 +20,13 @@ public class HeterogenousPool implements StoragePool {
     public SocketAddress getDaemon(int stripe) {
         return storageDaemons.get(stripe).get(replicationLayers.get(stripe));
     }
-    public List<SocketAddress> getStripe(int stripe) {
+    public List<SocketAddress> getStripe(int stripe) {//gets all daemons in stripe
         return storageDaemons.get(stripe);
     }
     public SocketAddress getDaemonCommandAddress(int stripe){
         return storageDaemonsCommandAddressses.get(stripe).get(replicationLayers.get(stripe));
     }
-    public void addStripe(SocketAddress[] stripeDaemons, Integer tier) {
+    public void addStripe(SocketAddress[] stripeDaemons, Integer tier) {//adds a homogenous stripe
         storageDaemons.add(Arrays.asList(stripeDaemons));
         tiers.add(tier);
        if(tierSizes.get(tier)==null){//checks to see if the tier is in the tierSizes hashmap already
@@ -37,10 +37,10 @@ public class HeterogenousPool implements StoragePool {
        }
         replicationLayers.add(new Integer(0));
     }
-    public void addStripe(SocketAddress[] stripeDaemons) {
+    public void addStripe(SocketAddress[] stripeDaemons) {//adds a heterogenous stripe, not supported
         throw new UnsupportedOperationException();
     }
-    public void addRepLayer(SocketAddress[] stripeDaemons) {
+    public void addRepLayer(SocketAddress[] stripeDaemons) {//adds a single daemon to every stripe
         if (stripeDaemons.length == storageDaemons.size()) {
             for (int i = 0; i < storageDaemons.size(); i++) {
                 storageDaemons.get(i).add(stripeDaemons[i]);
@@ -50,16 +50,16 @@ public class HeterogenousPool implements StoragePool {
             throw UnsupportedOperationException;
         }
     }
-    public void addRepDaemon(int stripe, SocketAddress daemon){
+    public void addRepDaemon(int stripe, SocketAddress daemon){//adds a single daemon to one stripe
         storageDaemons.get(stripe).add(daemon);
     }
-    public void remove(int stripe) {
+    public void remove(int stripe) {//deletes a stripe
         storageDaemons.remove(stripe);
         replicationLayers.remove(stripe);
         tiers.remove(stripe);
         tierSizes.remove(tiers.get(stripe));
     }
-    public void replace(int stripe, int repLayer, String newDaemon) {
+    public void replace(int stripe, int repLayer, SocketAddress newDaemon) {
         storageDaemons.get(stripe).set(repLayer, newDaemon);
     }
     public void incrementRepLayer(int stripe) {
@@ -68,11 +68,11 @@ public class HeterogenousPool implements StoragePool {
     public List<Integer> returnTiers(){
         return tiers;
     }
-    public HashMap<Integer,Integer> returnTierSizes(){
+    public ConcurrentHashMap<Integer,Integer> returnTierSizes(){
         return tierSizes;
     }
     public ArrayList<SocketAddress> getAllDaemons(){
-        List<String> output=new ArrayList<String>();
+        ArrayList<SocketAddress> output=new ArrayList<SocketAddress>();
         for(int i=0;i<storageDaemons.size();i++){
             output.add(storageDaemons.get(i).get(replicationLayers.get(i)));
         }
