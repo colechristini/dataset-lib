f439f7cf7c5e589f9a02c5ace99210ffc5057b96
Cole Christini <cole.christini@gmail.com>
2018-11-19 16:40:09 -0500

    Simplified storage key to remove intermediary steps for generating the serialVersionUUID

--- dataset.storageprovider/src/tech/pod/dataset/storageprovider/StorageKey.java
+++ dataset.storageprovider/src/tech/pod/dataset/storageprovider/StorageKey.java
@@ -3,30 +3,30 @@ package tech.pod.dataset.storageprovider;
 import java.io.Serializable;
 import java.util.UUID;
 
-
-public class StorageKey implements Serializable{
-    int pool,stripe;
+public class StorageKey implements Serializable {
+    int pool, stripe;
     String hash;
     String name;
-    static final UUID uuid=UUID.randomUUID();
-    static Integer temp=Integer.parseInt(uuid.toString());
-    static int temp1=temp;
-    static long temp2=temp1;
-    static final long serialVersionUID=temp2;
-    StorageKey(int pool, int stripe, String name, String hash){
-        this.pool=pool;
-        this.stripe=stripe;
-        this.name=name;
-        this.hash=hash;
+    static final UUID uuid = UUID.randomUUID();
+    static final long serialVersionUID = Integer.parseInt(uuid.toString());
+
+    StorageKey(int pool, int stripe, String name) {
+        this.pool = pool;
+        this.stripe = stripe;
+        this.name = name;
+        hash = Integer.toHexString(this.hashCode());
     }
-    public int[] getPath(){
-        int[] list={pool,stripe};
+
+    public int[] getPath() {
+        int[] list = { pool, stripe };
         return list;
     }
-    public String getHash(){
+
+    public String getHash() {
         return hash;
     }
-    public UUID getUUID(){
+
+    public UUID getUUID() {
         return uuid;
     }
 }
